59b19092db615c3f135ba2eda903060691263cef
Mengbo-Li <li.me@wehi.edu.au>
2021-05-13 13:05:19 +1000

    test

--- R/noNA.R
+++ R/noNA.R
@@ -1,5 +1,5 @@
-noNA <- function(Yc) {
+noNA <- function(x) {
 
-   if (sum(is.na(Yc)) == 0) {    return(TRUE)    } else {    return(FALSE)    }
+   if (sum(is.na(x)) == 0) {    return(TRUE)    } else {    return(FALSE)    }
 
 }
